49fd471f8b99fbe0b58f1a7548197c4ff474cafe
wangyufeng <675822728@qq.com>
2021-03-01 20:28:22 +0800

    Initial commit

---
+++ src/main/java/com/myp/dota2omg/Dota2omgApplication.java
@@ -0,0 +1,13 @@
+package com.myp.dota2omg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dota2omgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dota2omgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myp/dota2omg/Dota2omgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myp.dota2omg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dota2omgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
